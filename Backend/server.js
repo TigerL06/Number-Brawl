76a7710d42240c1d31657d44be4acfff38944c29
Koch <110893121+TigerL06@users.noreply.github.com>
2024-09-06 10:10:25 +0200

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+let number
 
+Randomnumber()
 app.set('view engine', 'ejs')
-app.get("/", (req, res) =>{
-
+app.Post("/", (rep, res) =>{
+    
+    console.log(number)
 })
-
 app.listen(3000)
 
+function Randomnumber(){
+    number = Math.floor(Math.random() * 101);;
+    console.log(number)
+}
