6845c371ad30142654bd3fdbda109dc0cd9c4d84
Marmeladenbrot1 <N.goehl.inf21@stud.bbbaden.ch>
2024-09-06 11:02:09 +0200

    Chat Room
    Chat Room aus einem Tutorial, welches wir nun anpassen werden.

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,36 @@
-const express = require('express')
-const app = express()
-let number
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-Randomnumber()
-app.set('view engine', 'ejs')
-app.Post("/", (rep, res) =>{
-    
-    console.log(number)
-})
-app.listen(3000)
+app.set('view engine', 'ejs');
 
-function Randomnumber(){
-    number = Math.floor(Math.random() * 101);;
-    console.log(number)
-}
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('join_room', (room) => {
+        socket.join(room);
+        console.log(`User joined room: ${room}`);
+    });
+
+    socket.on('message', ({ room, message }) => {
+        io.to(room).emit('message', {
+            message,
+            name: 'Friend'
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    });
+});
+
+server.listen(6800, () => {
+    console.log('Server running on port 6800');
+});
