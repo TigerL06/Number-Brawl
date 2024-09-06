c2256706e7d456dc36bac7c16f94f2ab8cf36cf5
Marmeladenbrot1 <N.goehl.inf21@stud.bbbaden.ch>
2024-09-06 09:11:34 +0200

    Entfernen von unnötigen Codeteilen
    Entfernen von unnötigen Codeteilen

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,10 @@
 const express = require('express')
 const app = express()
 
-
 app.set('view engine', 'ejs')
-app.get("/", (rep, res) =>{
-    console.log("Here")
-    res.json({})
-    res.download()
-    res.render()
-})
+app.get("/", (req, res) =>{
 
-const router = require('./routes/users')
-app.use('/users', router)
+})
 
 app.listen(3000)
 
